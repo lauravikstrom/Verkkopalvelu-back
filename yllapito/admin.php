6d60405273bf65da5eb23c934ef8f70893c79b54
t8kohe02 <t8kohe02@students.oamk.fi>
2021-12-02 14:27:58 +0200

    uuden categorian lisäys

---
+++ yllapito/admin.php
@@ -0,0 +1,30 @@
+<?php
+require_once '../inc/functions.php';
+require_once '../inc/headers.php';
+
+$input = json_decode(file_get_contents('php://input'));
+$newcategory = filter_var($input->newcategory,FILTER_SANITIZE_STRING);
+
+$db = null;
+
+try {
+    $db = openDB();
+    $db = beginTransaction();
+    //lisätään kategoria
+    $sql = "insert into category (name) values
+    ('" .
+    filter_var($newcategory,FILTER_SANITIZE_STRING)
+
+.    "')";
+$category_id = executeInsert($db,$sql);
+$db->commit();
+
+header('HTTP/1.1 200 OK');
+$data = array('id' => $customer_id);
+echo json_encode($data);
+}
+catch ( PDOException $pdoex) {
+    $db->rollback();
+    returnError($pdoex);
+}
+
