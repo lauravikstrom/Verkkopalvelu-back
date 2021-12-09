3c4ef5a0e840b8e6c2f82edba71415cfe726a210
t8kohe02 <t8kohe02@students.oamk.fi>
2021-12-09 18:53:57 +0200

    addproduct.php päivitys

--- yllapito/addproduct.php
+++ yllapito/addproduct.php
@@ -7,7 +7,7 @@ require_once '../inc/headers.php';
 $input = json_decode(file_get_contents('php://input'));
 $pname = filter_var($input->name,FILTER_SANITIZE_STRING);
 $pprice = filter_var($input->price,FILTER_SANITIZE_STRING);
-/* $pimage = filter_var($input->image,FILTER_SANITIZE_STRING); */
+$pimage = filter_var($input->image,FILTER_SANITIZE_STRING); 
 $pcategory_id = filter_var($input->category_id,FILTER_SANITIZE_STRING);
 
 $db = null;
@@ -17,10 +17,11 @@ try {
     $db->beginTransaction();
 
     // Lisätään asiakas
-    $sql = "insert into product (name,price,category_id) values
+    $sql = "insert into product (name,price,image,category_id) values
     ('" .
     filter_var($pname,FILTER_SANITIZE_STRING) . "','" .
     filter_var($pprice,FILTER_SANITIZE_STRING) . "','" .
+    filter_var($pimage,FILTER_SANITIZE_STRING) . "','" .
     filter_var($pcategory_id,FILTER_SANITIZE_STRING)
 .    "')";
 
