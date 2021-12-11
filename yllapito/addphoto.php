17adbe2bd7c181f51e6f12790dc309ba0f1613e2
jessepesse <58948883+jessepesse@users.noreply.github.com>
2021-12-11 18:37:31 +0200

    Kuvan lisääminen oikeaan kansioon

--- yllapito/addphoto.php
+++ yllapito/addphoto.php
@@ -3,7 +3,7 @@
 require_once '../inc/functions.php';
 require_once '../inc/headers.php';
 
-$target_dir = "IMG/";
+$target_dir = "../IMG/";
 $target_file = $target_dir . basename($_FILES["File"]["name"]);
 $uploadOk = 1;
 $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
