47193ce5b3b5fc2be4f719d0187a47a17c15b5c5
lauravikstrom <81621759+lauravikstrom@users.noreply.github.com>
2021-12-11 19:40:58 +0200

    Asiakkaan tilausten näyttäminen

--- yllapito/orderedproducts.php
+++ yllapito/orderedproducts.php
@@ -9,6 +9,7 @@ try {
     selectAsJson($db, 'SELECT customer.id, customer.firstname, customer.lastname, product.id, product.name, product.image, product.price
     FROM customer, product, order_row, `order`
     WHERE product.id = order_row.product_id and customer.id = `order`.`customer_id` and `order`.id = order_row.order_id
+    GROUP BY `order`.id
     ORDER BY product.name ASC ;');
 
 
---
+++ yllapito/orderedproducts2.php
@@ -0,0 +1,21 @@
+<?php
+require_once '../inc/functions.php';
+require_once '../inc/headers.php';
+
+// Haetaan tuote-taulusta tuotteet category_id:n perusteella
+$uri = parse_url(filter_input(INPUT_SERVER, 'PATH_INFO'),PHP_URL_PATH);
+
+$parameters = explode('/',$uri);
+
+$order_id = $parameters[1];
+
+try {
+    $db = openDB();
+    selectAsJson($db, "SELECT customer.id, customer.firstname, customer.lastname, product.id, product.name, product.image, product.price
+    FROM customer, product, order_row, `order`
+    WHERE product.id = order_row.product_id and customer.id = `order`.`customer_id` and `order`.id = order_row.order_id and order_row.order.id = $order_id
+    ORDER BY product.name ASC ;");
+}
+catch (PDOException $pdoex) {
+    returnError($pdoex);
+}
