62b10588bbe16a386b3d25dbfff17e48b56257e8
lauravikstrom <81621759+lauravikstrom@users.noreply.github.com>
2021-12-15 20:04:05 +0200

    Keskeneräinen Haku-toiminto

--- tuote/search.php
+++ tuote/search.php
@@ -4,24 +4,31 @@ require_once '../inc/headers.php';
 
 $input = json_decode(file_get_contents('php://input'));
 
-/* $uri = parse_url(filter_input(INPUT_SERVER, 'PATH_INFO'),PHP_URL_PATH);
+$uri = parse_url(filter_input(INPUT_SERVER, 'PATH_INFO'),PHP_URL_PATH);
 
-$keywords = explode(' ', $uri); */
+$parameters = explode('/', $uri);
+$phrase = $parameters[1];
+
+/* $keywords = explode(' ', $uri); 
 $searchTermKeywords = array();
 foreach ($input as $word) 
 {
 
 $searchTermKeywords[] = "name LIKE '%$word%'";
 
-}
+} */
 
 // Palauttaa haetut tuotteet fronttiin
 
 try {
     $db = openDB();
-    selectAsJson($db, "SELECT  * FROM product
+    
+    $sql = "select * from product where name like '%$phrase%'";
+    selectAsJson($db, $sql);
+    
+/*     selectAsJson($db, "SELECT  * FROM product
     WHERE ".implode(' AND ', $searchTermKeywords)."
-    ORDER BY name ASC");
+    ORDER BY name ASC"); */
 }
 catch (PDOException $pdoex) {
     returnError($pdoex);
