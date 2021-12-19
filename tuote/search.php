67d8b9b7b719253111e842fb183ef8dd51df4484
Anne <n0koan00@students.oamk.fi>
2021-12-19 13:11:40 +0200

    delete product

--- tuote/search.php
+++ tuote/search.php
@@ -10,20 +10,12 @@ $uri = parse_url(filter_input(INPUT_SERVER, 'PATH_INFO'),PHP_URL_PATH);
 $parameters = explode('/', $uri);
 $phrase = $parameters[1];
 
-<<<<<<< HEAD
-
-
-=======
->>>>>>> 8c7f9c714c8ad884b7fa743b8bf84f80048a0354
 // Palauttaa haetut tuotteet fronttiin
 try {
     $db = openDB();
     $sql = "select * from product where name like '%$phrase%'";
     selectAsJson($db, $sql);
-<<<<<<< HEAD
-=======
     
->>>>>>> 8c7f9c714c8ad884b7fa743b8bf84f80048a0354
 }
 catch (PDOException $pdoex) {
     returnError($pdoex);
